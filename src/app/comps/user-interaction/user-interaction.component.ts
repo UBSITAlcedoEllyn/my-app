15ea100942df019c52afa8a22baaa4e324d23b33
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-02-16 08:42:43 +0800

    Task 2: Implement Event Binding in Another Component

--- src/app/comps/user-interaction/user-interaction.component.ts
+++ src/app/comps/user-interaction/user-interaction.component.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './user-interaction.component.html',
   styleUrl: './user-interaction.component.css'
 })
-export class UserInteractionComponent {
 
+export class UserInteractionComponent {
+  message: string = 'Click the button!';
+  clickCount: number = 0;
+
+  onButtonClick(): void {
+    this.clickCount++;
+    this.message = `Button clicked ${this.clickCount} times!`;
+  }
+
+  onMouseEnter(): void {
+    console.log('Mouse entered the div!');
+  }
+
+  onInputChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    console.log('Input changed:', target.value);
+  }
 }
+
+
