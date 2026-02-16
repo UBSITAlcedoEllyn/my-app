2819f6eae16dc67912bb1575ce53183b6763e1d6
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-02-16 08:37:27 +0800

    created user-interaction comp

---
+++ src/app/user-interaction/user-interaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-interaction',
+  imports: [],
+  templateUrl: './user-interaction.component.html',
+  styleUrl: './user-interaction.component.css'
+})
+export class UserInteractionComponent {
+
+}
